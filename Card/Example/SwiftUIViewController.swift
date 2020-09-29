70e8d4295c514fee4bdf9974f4cf795754886a9d
Wei-Lun Su <allensu212@gmail.com>
2020-09-28 20:14:53 -0400

    feature: implemented tabBarController that shows UIKit and SwiftUI tab

---
+++ Card/Example/SwiftUIViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftUIViewController.swift
+//  Card
+//
+//  Created by Wei-Lun Su on 9/28/20.
+//  Copyright © 2020 Wei-Lun Su. All rights reserved.
+//
+
+import UIKit
+
+class SwiftUIViewController: UIViewController {
+    //
+}
